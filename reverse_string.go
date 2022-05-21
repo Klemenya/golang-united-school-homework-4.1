c33fa367239243ece5ff11ec61028bc207047e15
Pavel <klemenya@gmail.com>
2022-05-21 11:44:24 +0300

    Add task 4.1

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,11 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
+	temp := []rune(input)
+	n := len(temp)
+	for i := 0; i < n/2; i++ {
+		temp[i], temp[n-1-i] = temp[n-1-i], temp[i]
+	}
+	output = string(temp)
 	return output
 }
